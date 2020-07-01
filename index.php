66f496e1402e524105529ab3dbdb0f86bf91af2f
Manas Sunil Joshi <40157891+manasjoshi1@users.noreply.github.com>
2020-07-01 21:25:45 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Simmi Foundation</title>
+    <?php include('head-tag.php'); ?>
+  </head>
+  <body>
+
+    <?php include('nav1.php'); ?>
+
+    <div class="hero-wrap" style="background-image: url('images/bg_7.jpg');" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
+          <div class="col-md-7 ftco-animate text-center" data-scrollax=" properties: { translateY: '70%' }">
+            <h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Doing Nothing is Not An Option of Our Life</h1>
+
+
+            <p data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><a href="https://vimeo.com/45830194" class="btn btn-white btn-outline-white px-4 py-3 popup-vimeo"><span class="icon-play mr-2"></span>Watch Video</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section class="ftco-counter ftco-intro" id="section-counter">
+    	<div class="container">
+    		<div class="row no-gutters">
+    			<div class="col-md-5 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18 color-1 align-items-stretch">
+              <div class="text">
+              	<span>Collaborate</span>
+                <p>Simmi Foundation serves in the collaboration with schools, colleges and other institutions. Contact us to collaborate.</p>
+				<p><a href="contact.php" target="_blank" class="btn btn-white px-3 py-2 mt-2">Collaborate With Us</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18 color-2 align-items-stretch">
+              <div class="text">
+              	<h4 class="mb-2">Donate Money</h4>
+              	<p>Your donation will further our mission of ensuring a happy and healthy life of those in need.</p>
+              	<p><a href="donate.php" target="_blank" class="btn btn-white px-3 py-2 mt-2">Donate Now</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18 color-3 align-items-stretch">
+              <div class="text">
+              	<h3 class="mb-4">Be a Volunteer</h3>
+              	<p>Even the all-powerful Pointing has no control about the blind texts.</p>
+              	<p><a href="volunteer.php" target="_blank" class="btn btn-white px-3 py-2 mt-2">Be A Volunteer</a></p>
+              </div>
+            </div>
+          </div>
+    		</div>
+    	</div>
+    </section>
+
+    <section class="ftco-section">
+    	<div class="container">
+    		<div class="row">
+          <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 d-flex services p-3 py-4 d-block">
+              <div class="icon d-flex mb-3"><span><i class="fa fa-female"></i></span></div>
+              <div class="media-body pl-4">
+                <h3 class="heading"><a href="Khel-Sanghthan.php" class="campaign-a" style="color:black">Khel Sanghthan</h3>
+                <p>We provide vocational training facilities for women to enhance their craft skills.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 d-flex services p-3 py-4 d-block">
+              <div class="icon d-flex mb-3"><span><i class="fa fa-graduation-cap"></i></span></div>
+              <div class="media-body pl-4">
+                <h3 class="heading"><a href="Sukoon-Parikram.php" class="campaign-a" style="color:black">Sukoon Parikram</h3>
+                <p>We provide free academic education, scholarship, training and other incentives to children.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 d-flex services p-3 py-4 d-block">
+              <div class="icon d-flex mb-3"><span><i class="fa fa-bicycle"></i></span></div>
+
+              <div class="media-body pl-4">
+                <h3 class="heading"><a href="Simmi-Olympiads.php" class="campaign-a" style="color:black"> Simmi Olympiads</h3>
+                <p>We organize and conduct sports events and provide specialized coaching facilties for various sports activities.
+                    </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 d-flex services p-3 py-4 d-block">
+              <div class="icon d-flex mb-3"><span><i class="fa fa-rupee"></i></span></div>
+              <div class="media-body pl-4">
+                <h3 class="heading"><a href="Kalaakaar-manch.php" class="campaign-a" style="color:black">Kalaakaar Manch </h3>
+                <p>We implement various schemes in provide livelihood and uplift the poor in society.</p>
+              </div>
+            </div>
+          </div>
+              <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+                <div class="media block-6 d-flex services p-3 py-4 d-block">
+                  <div class="icon d-flex mb-3"><span><i class="fa fa-building"></i></span></div>
+                  <div class="media-body pl-4">
+                    <h3 class="heading"><a href="Sanskritik-Setu.php" class="campaign-a" style="color:black">Sanskritik Setu</h3>
+                    <p>We work in revive, repair and develop roads, libraries, community halls etc for the rural areas.</p>
+                  </div>
+                </div>
+              </div>
+            <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+              <div class="media block-6 d-flex services p-3 py-4 d-block">
+                <div class="icon d-flex mb-3"><span><i class="fa fa-ambulance"></i></span></div>
+                <div class="media-body pl-4">
+                  <h3 class="heading"> <a href="Sambhav-Madad.php" class="campaign-a" style="color:black">Sambhav Madad</a> </h3>
+                  <p>We are increasing awareness on Sanitization and providing better nutrition, clean water and toilets. </p>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <section class="ftco-section bg-light">
+    	<div class="container-fluid">
+    		<div class="row justify-content-center mb-5 pb-3">
+          <div class="col-md-5 heading-section ftco-animate text-center">
+            <h2 class="mb-4">Recent Events</h2>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          </div>
+        </div>
+    		<div class="row justify-content-center">
+        <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+
+	    			<div class="item">
+	    				<div class="cause-entry">
+		    				<a href="#" class="img" style="background-image: url(images/cause-1.jpg);"></a>
+		    					<div class="text p-3 p-md-4">
+		    						<h3><a href="#">Clean water for the urban area</a></h3>
+		    						<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life</p>
+		    						<span class="donation-time mb-3 d-block">Last donation 1w ago</span>
+		                <div class="progress custom-progress-success">
+		                  <div class="progress-bar bg-primary" role="progressbar" style="width: 28%" aria-valuenow="28" aria-valuemin="0" aria-valuemax="100"></div>
+		                  </div>
+		                <span class="fund-raised d-block">$12,000 raised of $30,000</span>
+		    					  </div>
+		    				  </div>
+	    				</div>
+            </div>
+              
+              <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+	    				<div class="item">
+	    					<div class="cause-entry">
+		    					<a href="#" class="img" style="background-image: url(images/cause-2.jpg);"></a>
+		    					<div class="text p-3 p-md-4">
+		    						<h3><a href="#">Clean water for the urban area</a></h3>
+		    						<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life</p>
+		    						<span class="donation-time mb-3 d-block">Last donation 1w ago</span>
+		                <div class="progress custom-progress-success">
+		                  <div class="progress-bar bg-primary" role="progressbar" style="width: 28%" aria-valuenow="28" aria-valuemin="0" aria-valuemax="100"></div>
+		                </div>
+		                <span class="fund-raised d-block">$12,000 raised of $30,000</span>
+		    					</div>
+		    				</div>
+	    				</div>
+              </div>
+              
+              <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+	    				<div class="item">
+	    					<div class="cause-entry">
+		    					<a href="#" class="img" style="background-image: url(images/cause-6.jpg);"></a>
+		    					<div class="text p-3 p-md-4">
+		    						<h3><a href="#">Clean water for the urban area</a></h3>
+		    						<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life</p>
+		    						<span class="donation-time mb-3 d-block">Last donation 1w ago</span>
+		                <div class="progress custom-progress-success">
+		                  <div class="progress-bar bg-primary" role="progressbar" style="width: 28%" aria-valuenow="28" aria-valuemin="0" aria-valuemax="100"></div>
+		                </div>
+		                <span class="fund-raised d-block">$12,000 raised of $30,000</span>
+		    					</div>
+		    				</div>
+	    				</div>
+            </div>
+
+    			
+    		</div>
+    	</div>
+    </section>
+
+    <!-- <section class="ftco-section">
+      <div class="container">
+      	<div class="row justify-content-center mb-5 pb-3">
+          <div class="col-md-7 heading-section ftco-animate text-center">
+            <h2 class="mb-4">Featured Partners</h2>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          </div>
+        </div>
+        <div class="row">
+        	<div class="col-lg-4 d-flex mb-sm-4 ftco-animate">
+        		<div class="staff">
+        			<div class="d-flex mb-4">
+        				<div class="img" style="background-image: url(images/person_1.jpg);"></div>
+        				<div class="info ml-4">
+        					<h3><a href="teacher-single.html">Ivan Jacobson</a></h3>
+        					<span class="position">Donated Just now</span>
+        					<div class="text">
+		        				<p>Donated <span>$300</span> for <a href="#">Children Needs Food</a></p>
+		        			</div>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        	<div class="col-lg-4 d-flex mb-sm-4 ftco-animate">
+        		<div class="staff">
+        			<div class="d-flex mb-4">
+        				<div class="img" style="background-image: url(images/person_2.jpg);"></div>
+        				<div class="info ml-4">
+        					<h3><a href="teacher-single.html">Ivan Jacobson</a></h3>
+        					<span class="position">Donated Just now</span>
+        					<div class="text">
+		        				<p>Donated <span>$150</span> for <a href="#">Children Needs Food</a></p>
+		        			</div>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        	<div class="col-lg-4 d-flex mb-sm-4 ftco-animate">
+        		<div class="staff">
+        			<div class="d-flex mb-4">
+        				<div class="img" style="background-image: url(images/person_3.jpg);"></div>
+        				<div class="info ml-4">
+        					<h3><a href="teacher-single.html">Ivan Jacobson</a></h3>
+        					<span class="position">Donated Just now</span>
+        					<div class="text">
+		        				<p>Donated <span>$250</span> for <a href="#">Children Needs Food</a></p>
+		        			</div>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </div>
+      </div>
+    </section> -->
+
+    <?php include('footer.php'); ?>
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+
+  </body>
+</html>
